7e2ebc3c02b63fc1c5ff57c30236be346d995a5f
JeremyAs15 <jeremyastaiza2@gmail.com>
2026-09-24 20:52:48 -0500

    feat: endpoints para subtareas

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -6,4 +6,10 @@ urlpatterns = [
     path("health/", views.health),
     path("events/", views.EventListCreateView.as_view(), name="event-list-create"),
     path("events/<int:pk>/", views.EventDetailView.as_view(), name="event-detail"),
+    path(
+        "events/<int:event_id>/tasks/",
+        views.TaskListCreateView.as_view(),
+        name="task-list-create",
+    ),
+    path("tasks/<int:pk>/", views.TaskDetailView.as_view(), name="task-detail"),
 ]
